93664cc7d2e47892b70071fb677bb9ab6aef8c4c
TubbyTubTub <39911498+Aaylen@users.noreply.github.com>
2024-02-07 11:56:44 -0800

    Working!

--- server.js
+++ server.js
@@ -41,5 +41,8 @@ app.get('/*', function(req, res) {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Click here to open in browser: http://localhost:${PORT}`);
+  });
